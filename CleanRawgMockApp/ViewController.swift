2b90db751b00abd07c77a8aba20fd82739c2ea3d
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-08-21 20:08:36 +0700

    Initial Commit

---
+++ CleanRawgMockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanRawgMockApp
+//
+//  Created by Farhandika Zahrir Mufti guenia on 21/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
